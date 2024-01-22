57cc612309dbe1a8ea6296c16fe538c070836baa
farsya <137470257+iloveichigomashimaro@users.noreply.github.com>
2024-01-21 21:30:17 -0800

    Add files via upload

---
+++ praktek 6/praktek6.js
@@ -0,0 +1,26 @@
+//let number = 10 + "10";
+//console.log(number);
+//Diatas merupakan contoh Numeric To String Coercion.
+
+//let str = "42";
+//let num = ++str; //unary operator
+//console.log(num);
+//diatas merupakan contoh string to numeric coercion.
+
+//const birthYear = prompt("Tahun berapa kamu lahir ?");
+//const yourAge = 2024 - Number(birthYear);
+//console.log(yourAge);
+
+//let num = 42;
+//let str = String(num);
+//console.log(str);
+
+const age = 20;
+const height = 167;
+const weight = 60;
+const isStudent = true;
+
+const ageString = String(age);
+const heightString = height = "cm";
+const weightString = weight = "kg";
+const studentStatus = isStudent(numbers);
