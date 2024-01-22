ddeaa98751368415e07cdb1cf55a9301f91ab1af
farsya <137470257+iloveichigomashimaro@users.noreply.github.com>
2024-01-21 21:56:46 -0800

    Add files via upload

---
+++ praktek 7/asdksapraktek.js
@@ -0,0 +1,43 @@
+// let silaPancasila = 1;
+// switch (silaPancasila) {
+//     case 1:
+//         document.write("Ketuhanan yang maha esa");
+//         break;
+//     case 2:
+//         document.write("Kemanusian yang adil & beradab");
+//         break;
+//     case 3:
+//         document.write("Persatuan Indonesia");
+//         break;
+//     case 4:
+//         document.write("Kerakyatan yang dipimpin oleh hikmat kebijaksanaan dalam permusyawaratan perwakilan");
+//         break;
+//     case 5:
+//         document.write("Keadilan Sosial Bagi Seluruh Rakyat Indonesia");
+//         break;
+//}
+
+let kodePlatnomor;
+
+switch (kodePlatnomor) {
+    case "KU":
+        document.write("KU 7272 UH");
+    break;
+
+    case "KT":
+        document.write("KT 8906 DK");
+    break;
+
+    case "DA":
+        document.write("DA 7979 UO");
+    break;
+
+    case "KB":
+        document.write("KB 6473 WA");
+    break;
+
+    case "KH":
+        document.write("KH 9565 DM");
+    break;
+}
+
